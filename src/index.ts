7e6af6db1bf4d7ae24a2ae33c8b5a6f41e5d97d9
Gaurav Handa <gaurav@shinewebservices.com>
2021-02-06 18:50:04 +0530

    heroku changes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import logger from '@utilities/logger'
 
 logger.info(app)
 // Start the server
-const port = Number(process.env.PORT || 4000)
+const port: number = Number(process.env.PORT) || 4000
 const host: string = process.env.HOST || 'localhost'
 app.listen(port, host, () => {
     logger.info('asd Server started on port: ' + port)
